9f17c5f591310709d8c7988a2e75ee52ab0be2b8
Christos Nikolaou <me@christosnikolaou.org>
2023-08-12 16:54:26 +0100

    Add condition-builder functions on `AttestationQueryBuilder`
    Add functionality for building condition that can be run on the
    DataBase, as part of the `AttestationQueryBuilder` class.

--- src/attestation.ts
+++ src/attestation.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Schema } from '@prisma/client';
+import { Attestation, PrismaClient, Schema } from '@prisma/client';
 
 export class AttestationQueryBuilder {
   private condition: any = {};
@@ -18,14 +18,79 @@ export class AttestationQueryBuilder {
     if (!schema) {
       throw new Error(`Schema with UID ${schemaUid} not found.`);
     }
+
     return new AttestationQueryBuilder(schema, prismaClient);
   }
 
-  creator() {
+  ///
+  // Schema-related functions to return relevant data. These Schema data
+  // doesn't change so returning data from the in-memory `this.schema`
+  // object is fine.
+
+  schemaCreator() {
     return this.schema.creator;
   }
 
-  id() {
+  schemaUid() {
     return this.schema.id;
+  }
+
+  ///
+  // Condition-building functions
+
+  attesterIs(attester: string): this {
+    this.condition.attester = attester;
+    return this;
+  }
+
+  recipientIs(recipient: string): this {
+    this.condition.recipient = recipient;
+    return this;
+  }
+
+  beforeDate(date: Date): this {
+    const timestamp = Math.floor(date.getTime() / 1000);
+    console.log(timestamp);
+    this.condition.time = { lt: timestamp };
+    return this;
+  }
+
+  afterDate(date: Date): this {
+    const timestamp = Math.floor(date.getTime() / 1000);
+    this.condition.time = { gt: timestamp };
+    return this;
+  }
+
+  ///
+  // Functions that run the queries and return the DB results without any
+  // other processing. They can be used for introspection and to run more
+  // complex queries on the returned data.
+
+  async dbFindMany(): Promise<any[]> {
+    this.condition.schemaId = this.schemaUid();
+    return await this.prisma.attestation.findMany({
+      where: this.condition,
+    });
+  }
+
+  async dbFindFirst(): Promise<Attestation | null> {
+    this.condition.schemaId = this.schemaUid();
+    return await this.prisma.attestation.findFirst({
+      where: this.condition,
+    });
+  }
+
+  async dbFindUnique(): Promise<Attestation | null> {
+    this.condition.schemaId = this.schemaUid();
+    return await this.prisma.attestation.findUnique({
+      where: this.condition,
+    });
+  }
+
+  async dbCount(): Promise<number> {
+    this.condition.schemaId = this.schemaUid();
+    return await this.prisma.attestation.count({
+      where: this.condition,
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@ import { AttestationQueryBuilder } from './attestation';
 const prisma = new PrismaClient();
 
 async function main() {
-  const schemaUid = '0x016300465697c6908e2b6cee414c5b4d3261b172dfba2ffb427efc744fd3831b';
+  const schemaUid = '';
+  const attester = '';
+  const recipient = '';
 
   const schema = await AttestationQueryBuilder.create(schemaUid, prisma);
-  console.log(`Schema ID:\n${schema.id()}\nShould match:\n${schemaUid}`);
+  console.log(`Schema ID:\n${schema.schemaUid()}\nShould match:\n${schemaUid}`);
+
+  const countTest = await schema
+    .attesterIs(attester)
+    .recipientIs(recipient)
+    .beforeDate(new Date(2023, 7, 11, 23, 50, 50))
+    .dbCount();
+  console.log(countTest);
 }
 
 main()
