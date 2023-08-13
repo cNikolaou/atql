e3c5f6ce8dedb08e422dae857cda426730ec6133
Christos Nikolaou <me@christosnikolaou.org>
2023-08-13 10:43:32 +0100

    demo: Add missing `api/schema-create` endpoint

---
+++ demo/pages/api/schema-create.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createSchema } from '../../lib/attestationUtils';
+
+interface Data {
+  schemaUid: string;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method === 'POST' && req.body.schema) {
+    console.log('req.body');
+    console.log(req.body);
+    const schemaUid = await createSchema(req.body.schema);
+    res.status(200).json({ schemaUid: schemaUid });
+  }
+}
